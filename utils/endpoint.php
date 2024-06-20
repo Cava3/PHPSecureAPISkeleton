4a803ca3eea65e27c35bde0c55e233cae04591f6
Roucool <lucastag77@gmail.com>
2024-06-20 13:37:53 +0200

    [W] Endpoint util almost finished

--- utils/endpoint.php
+++ utils/endpoint.php
@@ -1,25 +1,70 @@
 <?php
 
-function writeToDatabase($value) {
+$GLOBALS["ERROR_CODES"] = array(
+    600 => "SID not found",
+    601 => "SID is outdated",
+    602 => "SID not provided"
+);
+
+function getError($code): array {
+    return array("error" => $code, "message" => $GLOBALS["ERROR_CODES"][$code]);
+}
+
+function beginEndpoint(): bool {
     // Get the database connection details from the environment variable
     $mysqlHost = getenv('MYSQL_CONTAINER');
     $mysqlDatabase = getenv('MYSQL_DATABASE');
     $mysqlUsername = getenv('MYSQL_USER');
     $mysqlPassword = getenv('MYSQL_PASSWORD');
 
+    // Check if the SID is provided
+    if (!isset($_GET['sid'])) {
+        header("Content-Type: application/json");
+        echo json_encode(getError(602));
+        return false;
+    }
+
+    $SID = $_GET['sid'];
+
     // Create a new PDO instance
     $fullURI = "mysql:host=$mysqlHost;dbname=$mysqlDatabase";
     $connection = new PDO($fullURI, $mysqlUsername, $mysqlPassword);
 
-    // // Prepare and execute the SQL query
-    // $request = $connection->prepare("INSERT INTO your_table_name (column_name) VALUES (:value)");
-    // $request->bindParam(':value', $value);
-    // $request->execute();
+    // Check if SID is correct and not outdated
+    $query = "SELECT last_seen FROM user WHERE sid = :sid";
+    $statement = $connection->prepare($query);
+    $statement->bindParam(':sid', $SID);
+    $statement->execute();
+
+    $result = $statement->fetch(PDO::FETCH_ASSOC);
+    if (!$result) {
+        header("Content-Type: application/json");
+        echo json_encode(getError(600));
+        return false;
+    }
+
+    $lastSeen = $result[0]['last_seen'];
+    $currentTime = time();
+
+    if ($currentTime - $lastSeen > 3600) {
+        header("Content-Type: application/json");
+        echo json_encode(getError(601));
+        return false;
+    }
+
+    // Update the last seen time
+    $query = "UPDATE user SET last_seen = :last_seen WHERE sid = :sid";
+    $statement = $connection->prepare($query);
+    $statement->bindParam(':last_seen', $currentTime);
+    $statement->bindParam(':sid', $SID);
+    $statement->execute();
+
+    $result = $statement->fetch(PDO::FETCH_ASSOC);
 
     // Close the database connection
     $connection = null;
+    return true;
 }
 
-// Usage example
-$value = "Hello, world!";
-writeToDatabase($value);
+// TODO: Clean
+beginEndpoint();
