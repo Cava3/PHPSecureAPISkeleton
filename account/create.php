4556a9866937d1d41f470be78535adef19f21ffc
Roucool <lucastag77@gmail.com>
2024-06-25 15:52:59 +0200

    [NT] Account creation

---
+++ account/create.php
@@ -0,0 +1,67 @@
+<?php
+
+// Get the database connection details from the environment variable
+$mysqlHost = getenv('MYSQL_CONTAINER');
+$mysqlDatabase = getenv('MYSQL_DATABASE');
+$mysqlUsername = getenv('MYSQL_USER');
+$mysqlPassword = getenv('MYSQL_PASSWORD');
+
+// Create a new PDO instance
+$fullURI = "mysql:host=$mysqlHost;dbname=$mysqlDatabase";
+$connection = new PDO($fullURI, $mysqlUsername, $mysqlPassword);
+
+// Check if username and password are provided
+if (!isset($_POST['username']) || !isset($_POST['password']) || !isset($_POST['email'])) {
+    sendError(604);
+}
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+// Derivate a salt from the username
+$salt = substr(hash('sha256', $username), 0, 6);
+
+// Hash the password
+$hashedPassword = password_hash($password, PASSWORD_BCRYPT);
+
+// Check if the username and password combination already exists
+$query = "SELECT * FROM user WHERE username = :username AND passwd = :passwd";
+$statement = $connection->prepare($query);
+$statement->bindParam(':username', $username);
+$statement->bindParam(':passwd', $hashedPassword);
+$statement->execute();
+
+$uniqueCheckResult = $statement->fetch(PDO::FETCH_ASSOC);
+if ($uniqueCheckResult) {
+    sendError(606);
+}
+
+// Generate a new SID (64 chars long)
+$SIDresult = false;
+$SID = "";
+
+do {
+    $seed = rand();
+    $SID = hash('sha256', $seed);
+
+    $query = "SELECT * FROM user WHERE sid = :sid";
+    $statement = $connection->prepare($query);
+    $statement->bindParam(':sid', $SID);
+    $statement->execute();
+    $SIDresult = $statement->fetch(PDO::FETCH_ASSOC);
+} while ($SIDresult || strlen($SID) != 64);
+
+// Insert the new user into the database
+$query = "INSERT INTO `user` (`username`, `passwd`, `sid`, `last_seen`, `salt`, `email`) VALUES (:username, :passwd, :sid, NOW(), :salt, :email)";
+$statement = $connection->prepare($query);
+$statement->bindParam(':username', $username);
+$statement->bindParam(':passwd', $hashedPassword);
+$statement->bindParam(':sid', $SID);
+$statement->bindParam(':salt', $salt);
+$statement->bindParam(':email', $_POST['email']);
+$statement->execute();
+
+// Return the SID
+header("Content-Type: application/json");
+echo json_encode(array("sid" => $SID));
+exit;
